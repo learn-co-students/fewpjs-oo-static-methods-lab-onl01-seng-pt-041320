efaf55ec2e20b796c7766c0c62d8d1c31e4db111
Dice-K39 <daisukehashimoto39@gmail.com>
2020-11-15 00:52:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
-class Formatter {
+class Formatter 
+{
   //add static methods here
+  static capitalize(string)
+  {
+    return (string.length === 1) ? string.charAt(0).toUpperCase() : string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string)
+  {
+    let newString = [];
+
+    for (let i = 0; i < string.length; i++)
+    {
+      if (string[i].match(/[a-zA-Z-' ]/))
+      {
+        newString.push(string[i]);
+      }
+    }
+
+    return newString.join('');
+  }
+
+  static titleize(string)
+  {
+    const noCapWords = ["the", 'a', "an", "but", "of", "and", "for", "at", "by", "from"];
+    const wordArray = string.split(' ');
+    let newString = [];
+
+    for (let i = 0; i < wordArray.length; i++)
+    {
+      if (i === 0)
+      {
+        newString.push(this.capitalize(wordArray[i]));
+      }
+      else
+      {
+        if (noCapWords.includes(wordArray[i]))
+        {
+          newString.push(wordArray[i]);
+        }
+        else
+        {
+          newString.push(this.capitalize(wordArray[i]));
+        }
+      }
+    }
+
+    return newString.join(' ');
+  }
 }
