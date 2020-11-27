b8f7ce084c9afe999d8e31ce7f8b06bfa519b812
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-11-27 10:48:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    const tooSmall = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let newSentence = string.split(" ")[0].charAt(0).toUpperCase() + string.slice(1)
+    newSentence = newSentence.split(" ")
+    for (let i = 1; i < newSentence.length; i++) {
+      if (!tooSmall.includes(newSentence[i])) {
+        let change = newSentence[i].charAt(0).toUpperCase() + newSentence[i].slice(1)
+        newSentence.splice(i, 1, change)
+        newSentence.join(" ")
+      }
+    }
+    return newSentence.join(" ")
+  }
 }
