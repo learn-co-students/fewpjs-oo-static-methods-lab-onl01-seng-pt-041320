6e102098650e155e609dd7cd3deefdb5f10ce8bb
gtblackmore <gtblackmore@gmail.com>
2021-03-20 18:22:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string)
+  {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string)
+  {
+    let newString = [];
+
+    for (let i = 0; i < string.length; i++)
+    {
+      if (string[i].match(/[a-zA-Z-' ]/))
+      {
+        newString.push(string[i]);
+      }
+    }
+
+    return newString.join('');
+  }
+
+  static titleize(string)
+  {
+    const exceptions = ["the", 'a', "an", "but", "of", "and", "for", "at", "by", "from"];
+    const wordArray = string.split(' ');
+    let newString = [];
+
+    for (let i = 0; i < wordArray.length; i++)
+    {
+      if (i === 0 || !exceptions.includes(wordArray[i]))
+      {
+        newString.push(this.capitalize(wordArray[i]));
+      }
+      else
+      {
+        newString.push(wordArray[i]);
+      }
+    }
+
+    return newString.join(' ');
+  }
 }
